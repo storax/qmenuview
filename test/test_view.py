2cae3a623bce4336f55ef8ec12f1de1dcfb8a637
David Zuber <zuber.david@gmx.de>
2015-08-06 01:10:25 +0200

    Add first simple title and parent test

--- test/test_view.py
+++ test/test_view.py
@@ -1,1 +1,15 @@
-import pytest
+from PySide import QtGui
+
+import qmenuview
+
+
+def test_title(qtbot):
+    title = 'Test title'
+    qmenuview.MenuView(title)
+    assert qmenuview.title() == title
+
+
+def test_parent(qtbot):
+    p = QtGui.QWidget()
+    qmenuview.MenuView(parent=p)
+    assert qmenuview.parent() is p
