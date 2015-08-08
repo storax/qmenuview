46667da0bb2167bd1a285fe02d6e9b060b1f2645
David Zuber <zuber.david@gmx.de>
2015-08-08 13:23:57 +0200

    Change order of methods

--- src/qmenuview/view.py
+++ src/qmenuview/view.py
@@ -102,102 +102,6 @@ n
         """A list of :class:`SetDataArgs` containers. Defines how the
         data from the model is applied to the action"""
 
-    def get_index(self, action, column=0):
-        """Return the index for the given action
-
-        :param action: the action to query
-        :type action: :class:`QtGui.QAction`
-        :param column: The column of the index
-        :type column: :class:`int`
-        :returns: the index of the action
-        :rtype: :class:`QtCore.QModelIndex`
-        :rasies: None
-        """
-        if action == self.menuAction():
-            return QtCore.QModelIndex()
-        # find all parents to get their index
-        parents = self._get_parents(action)
-        index = QtCore.QModelIndex()
-        # Move through indexes down the chain
-        for a in reversed(parents):
-            parent = a.parent()
-            # if parent of action is its own menu, get parent of that menu.
-            # We want to know which row the action is in. For that we need
-            # The real parent menu.
-            if parent is a.menu():
-                parent = parent.parent()
-            row = parent.actions().index(a)
-            index = self._model.index(row, 0, index)
-        parent = action.parent()
-        if parent is None:
-            return index
-        if parent is action.menu():
-            parent = parent.parent()
-        row = parent.actions().index(action)
-        index = self._model.index(row, column, index)
-        return index
-
-    def get_action(self, index):
-        """Return the action for the given index
-
-        :param index: the index to query
-        :type index: :class:`QtCore.QModelIndex`
-        :returns: the action for the given index
-        :rtype: :class:`QtGui.QAction`
-        :raises: None
-        """
-        if not index.isValid():
-            return self.menuAction()
-        parents = self._get_parent_indizes(index)
-        menu = self
-        for i in reversed(parents):
-            action = menu.actions()[i.row()]
-            menu = action.menu()
-        try:
-            return menu.actions()[index.row()]
-        except IndexError:
-            return None
-
-    def _get_parents(self, action):
-        parents = []
-        a = action
-        while True:
-            parent = a.parent()
-            # If parent is self, then the action is the views menuAction
-            # So there are no parents
-            if parent is self:
-                return []
-            # if the parent is the actions menu, we have to get
-            # the menues parent. Else we get stuck on the same level.
-            if parent and parent is a.menu():
-                parent = parent.parent()
-            if not isinstance(parent, QtGui.QMenu):
-                # Is not part of the tree. Parent is not a menu but
-                # might be None or another Widget
-                return []
-            # break if parent is root because we got all parents we need
-            if parent is self:
-                break
-            # a new parent was found and we are still not at root
-            # search further until we get to root
-            parent = parent.menuAction()
-            a = parent
-            parents.append(parent)
-        return parents
-
-    def _get_parent_indizes(self, index):
-        if not index.isValid() or index.model() != self._model:
-            return []
-        parents = []
-        i = index
-        while True:
-            p = i.parent()
-            if not p.isValid():
-                break
-            parents.append(p)
-            i = p
-        return parents
-
     @property
     def model(self, ):
         """Get the model
@@ -239,9 +143,9 @@ n
         :raises: None
         """
         self.clear()
-        self._create_all_menus()
+        self.create_all_menus()
 
-    def _create_all_menus(self, ):
+    def create_all_menus(self, ):
         """Create all menus according to the model
 
         :returns: None
@@ -308,6 +212,11 @@ n
         for signal, callback in signalmap.items():
             signal.connect(functools.partial(callback, action))
 
+    def _convert_action_to_menu(self, action):
+        parent = action.parentWidget()
+        menuaction = self.create_menu(parent)
+        action.setMenu(menuaction.menu())
+
     def create_menu(self, parent):
         """Create a menu and return the menus action.
 
@@ -334,146 +243,6 @@ n
         :raises: None
         """
         return QtGui.QAction(parent)
-
-    def set_action_data(self, action, index):
-        """Set the data of the action for the given index
-
-        .. Note:: The column of the index does not matter. The columns for the data
-                  are specified in :data:`MenuView.setdataargs`.
-
-        The arguments to used are defined in :data:`MenuView.setdataargs`.
-
-        :param action: The action to update
-        :type action: :class:`QtGui.QAction`
-        :param index: The index with the data
-        :type index: :class:`QtCore.QModelIndex`
-        :returns: None
-        :rtype: None
-        :raises: None
-        """
-        self._set_action_enabled(action, index)
-        self._set_action_checkable(action, index)
-        for args in self.setdataargs:
-            self._set_action_attribute(action, index, args)
-
-    @staticmethod
-    def _checkconvertfunc(data):
-        checkedstate = int(data) if data is not None else 0
-        return checkedstate == QtCore.Qt.Checked
-
-    def _set_action_enabled(self, action, index):
-        """Enable the action , depending on the item flags
-
-        :param action: The action to update
-        :type action: :class:`QtGui.QAction`
-        :param index: the model index with the item flags
-        :type index: :class:`QtCore.QModelIndex`
-        :returns: None
-        :rtype: None
-        :raises: None
-        """
-        action.setEnabled(index.flags() & QtCore.Qt.ItemIsEnabled)
-
-    def _set_action_checkable(self, action, index):
-        """Set the action checkable, depending on the item flags
-
-        .. Note:: The column of the index does not matter. The column for the data
-                  is specified in :data:`MenuView.checked_column`.
-
-        :param action: The action to update
-        :type action: :class:`QtGui.QAction`
-        :param index: the model index with the item flags
-        :type index: :class:`QtCore.QModelIndex`
-        :returns: None
-        :rtype: None
-        :raises: None
-        """
-        checkedindex = index.sibling(index.row(), self.checked_column)
-        checkedflags = checkedindex.flags()
-        action.setCheckable(checkedflags & QtCore.Qt.ItemIsUserCheckable)
-
-    def _set_action_attribute(self, action, index, setdataarg):
-        """Query the data of index and use it to set an attribute on action.
-
-        .. Note:: The column of the index does not matter. The column for the data
-                  is specified in ``setdataarg.column``.
-
-        The data will be converted with ``setdataarg.convertfunc``.
-
-        :param action: the action to update
-        :type action: :class:`QtGui.QAction`
-        :param index: the index with the data
-        :type index: :class:`QtCore.QModelIndex`
-        :param setdataarg: The container with arguments that define the way the data is applied
-        :type setdataarg: :class:`SetDataArgs`
-        :returns: None
-        :rtype: None
-        :raises: None
-        """
-        data = self.get_data(index, setdataarg.role, setdataarg.column)
-        if data is None:
-            return
-        setattrmethod = getattr(action, setdataarg.setfunc)
-        if setdataarg.convertfunc:
-            data = setdataarg.convertfunc(data)
-        setattrmethod(data)
-
-    def _convert_action_to_menu(self, action):
-        parent = action.parentWidget()
-        menuaction = self.create_menu(parent)
-        action.setMenu(menuaction.menu())
-
-    def _action_hovered(self, action):
-        """Emit the hovered signal
-
-        :param action: The action which emitted a hovered signal
-        :type action: :class:`QtGui.QAction`
-        :returns: None
-        :rtype: None
-        :raises: None
-        """
-        self._emit_signal_for_action(self.action_hovered, action)
-
-    def _action_triggered(self, action, checked=False):
-        """Emit the triggered signal
-
-        :param action: The action which emitted a triggered signal
-        :type action: :class:`QtGui.QAction`
-        :param checked: True if the action was in a checked state
-        :type checked: :class:`bool`
-        :returns: None
-        :rtype: None
-        :raises: None
-        """
-        self._emit_signal_for_action(self.action_triggered, action, checked)
-
-    def _action_toggled(self, action, checked=False):
-        """Emit the toggled signal
-
-        :param action: The action which emitted a toggled signal
-        :type action: :class:`QtGui.QAction`
-        :param checked: True if the action was in a checked state
-        :type checked: :class:`bool`
-        :returns: None
-        :rtype: None
-        :raises: None
-        """
-        self._emit_signal_for_action(self.action_toggled, action, checked)
-
-    def _emit_signal_for_action(self, signal, action, *args):
-        """Emit the given signal for the index of the given action
-
-        :param signal: The signal to emit
-        :type signal: :class:`QtCore.Signal`
-        :param action: The action for which to emit the signal
-        :type action: :class:`QtGui.QAction`
-        :returns: None
-        :rtype: None
-        :raises: None
-        """
-        index = self.get_index(action)
-        if index and index.isValid():
-            signal.emit(index, *args)
 
     def insert_menus(self, parent, first, last):
         """Create menus for rows first til last under the given parent
@@ -539,6 +308,180 @@ n
                 action = self.get_action(index)
                 self.set_action_data(action, index)
 
+    def get_index(self, action, column=0):
+        """Return the index for the given action
+
+        :param action: the action to query
+        :type action: :class:`QtGui.QAction`
+        :param column: The column of the index
+        :type column: :class:`int`
+        :returns: the index of the action
+        :rtype: :class:`QtCore.QModelIndex`
+        :rasies: None
+        """
+        if action == self.menuAction():
+            return QtCore.QModelIndex()
+        # find all parents to get their index
+        parents = self._get_parents(action)
+        index = QtCore.QModelIndex()
+        # Move through indexes down the chain
+        for a in reversed(parents):
+            parent = a.parent()
+            # if parent of action is its own menu, get parent of that menu.
+            # We want to know which row the action is in. For that we need
+            # The real parent menu.
+            if parent is a.menu():
+                parent = parent.parent()
+            row = parent.actions().index(a)
+            index = self._model.index(row, 0, index)
+        parent = action.parent()
+        if parent is None:
+            return index
+        if parent is action.menu():
+            parent = parent.parent()
+        row = parent.actions().index(action)
+        index = self._model.index(row, column, index)
+        return index
+
+    def _get_parents(self, action):
+        parents = []
+        a = action
+        while True:
+            parent = a.parent()
+            # If parent is self, then the action is the views menuAction
+            # So there are no parents
+            if parent is self:
+                return []
+            # if the parent is the actions menu, we have to get
+            # the menues parent. Else we get stuck on the same level.
+            if parent and parent is a.menu():
+                parent = parent.parent()
+            if not isinstance(parent, QtGui.QMenu):
+                # Is not part of the tree. Parent is not a menu but
+                # might be None or another Widget
+                return []
+            # break if parent is root because we got all parents we need
+            if parent is self:
+                break
+            # a new parent was found and we are still not at root
+            # search further until we get to root
+            parent = parent.menuAction()
+            a = parent
+            parents.append(parent)
+        return parents
+
+    def get_action(self, index):
+        """Return the action for the given index
+
+        :param index: the index to query
+        :type index: :class:`QtCore.QModelIndex`
+        :returns: the action for the given index
+        :rtype: :class:`QtGui.QAction`
+        :raises: None
+        """
+        if not index.isValid():
+            return self.menuAction()
+        parents = self._get_parent_indizes(index)
+        menu = self
+        for i in reversed(parents):
+            action = menu.actions()[i.row()]
+            menu = action.menu()
+        try:
+            return menu.actions()[index.row()]
+        except IndexError:
+            return None
+
+    def _get_parent_indizes(self, index):
+        if not index.isValid() or index.model() != self._model:
+            return []
+        parents = []
+        i = index
+        while True:
+            p = i.parent()
+            if not p.isValid():
+                break
+            parents.append(p)
+            i = p
+        return parents
+
+    def set_action_data(self, action, index):
+        """Set the data of the action for the given index
+
+        .. Note:: The column of the index does not matter. The columns for the data
+                  are specified in :data:`MenuView.setdataargs`.
+
+        The arguments to used are defined in :data:`MenuView.setdataargs`.
+
+        :param action: The action to update
+        :type action: :class:`QtGui.QAction`
+        :param index: The index with the data
+        :type index: :class:`QtCore.QModelIndex`
+        :returns: None
+        :rtype: None
+        :raises: None
+        """
+        self._set_action_enabled(action, index)
+        self._set_action_checkable(action, index)
+        for args in self.setdataargs:
+            self._set_action_attribute(action, index, args)
+
+    def _set_action_enabled(self, action, index):
+        """Enable the action , depending on the item flags
+
+        :param action: The action to update
+        :type action: :class:`QtGui.QAction`
+        :param index: the model index with the item flags
+        :type index: :class:`QtCore.QModelIndex`
+        :returns: None
+        :rtype: None
+        :raises: None
+        """
+        action.setEnabled(index.flags() & QtCore.Qt.ItemIsEnabled)
+
+    def _set_action_checkable(self, action, index):
+        """Set the action checkable, depending on the item flags
+
+        .. Note:: The column of the index does not matter. The column for the data
+                  is specified in :data:`MenuView.checked_column`.
+
+        :param action: The action to update
+        :type action: :class:`QtGui.QAction`
+        :param index: the model index with the item flags
+        :type index: :class:`QtCore.QModelIndex`
+        :returns: None
+        :rtype: None
+        :raises: None
+        """
+        checkedindex = index.sibling(index.row(), self.checked_column)
+        checkedflags = checkedindex.flags()
+        action.setCheckable(checkedflags & QtCore.Qt.ItemIsUserCheckable)
+
+    def _set_action_attribute(self, action, index, setdataarg):
+        """Query the data of index and use it to set an attribute on action.
+
+        .. Note:: The column of the index does not matter. The column for the data
+                  is specified in ``setdataarg.column``.
+
+        The data will be converted with ``setdataarg.convertfunc``.
+
+        :param action: the action to update
+        :type action: :class:`QtGui.QAction`
+        :param index: the index with the data
+        :type index: :class:`QtCore.QModelIndex`
+        :param setdataarg: The container with arguments that define the way the data is applied
+        :type setdataarg: :class:`SetDataArgs`
+        :returns: None
+        :rtype: None
+        :raises: None
+        """
+        data = self.get_data(index, setdataarg.role, setdataarg.column)
+        if data is None:
+            return
+        setattrmethod = getattr(action, setdataarg.setfunc)
+        if setdataarg.convertfunc:
+            data = setdataarg.convertfunc(data)
+        setattrmethod(data)
+
     @staticmethod
     def get_data(index, role, column=None):
         """Get data of the given index
@@ -561,6 +504,58 @@ n
         if index.isValid():
             return index.data(role)
 
+    def _action_hovered(self, action):
+        """Emit the hovered signal
+
+        :param action: The action which emitted a hovered signal
+        :type action: :class:`QtGui.QAction`
+        :returns: None
+        :rtype: None
+        :raises: None
+        """
+        self._emit_signal_for_action(self.action_hovered, action)
+
+    def _action_triggered(self, action, checked=False):
+        """Emit the triggered signal
+
+        :param action: The action which emitted a triggered signal
+        :type action: :class:`QtGui.QAction`
+        :param checked: True if the action was in a checked state
+        :type checked: :class:`bool`
+        :returns: None
+        :rtype: None
+        :raises: None
+        """
+        self._emit_signal_for_action(self.action_triggered, action, checked)
+
+    def _action_toggled(self, action, checked=False):
+        """Emit the toggled signal
+
+        :param action: The action which emitted a toggled signal
+        :type action: :class:`QtGui.QAction`
+        :param checked: True if the action was in a checked state
+        :type checked: :class:`bool`
+        :returns: None
+        :rtype: None
+        :raises: None
+        """
+        self._emit_signal_for_action(self.action_toggled, action, checked)
+
+    def _emit_signal_for_action(self, signal, action, *args):
+        """Emit the given signal for the index of the given action
+
+        :param signal: The signal to emit
+        :type signal: :class:`QtCore.Signal`
+        :param action: The action for which to emit the signal
+        :type action: :class:`QtGui.QAction`
+        :returns: None
+        :rtype: None
+        :raises: None
+        """
+        index = self.get_index(action)
+        if index and index.isValid():
+            signal.emit(index, *args)
+
     @staticmethod
     def _process_icondata(icondata):
         """Return an icon for the data of the :data:`QtCore.Qt.DecorationRole`
@@ -575,3 +570,8 @@ n
             return icondata
         if isinstance(icondata, QtGui.QPixmap):
             return QtGui.QIcon(icondata)
+
+    @staticmethod
+    def _checkconvertfunc(data):
+        checkedstate = int(data) if data is not None else 0
+        return checkedstate == QtCore.Qt.Checked
